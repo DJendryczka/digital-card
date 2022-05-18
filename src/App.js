3a086aaea800a09d508b46628a5167dfc2a2a9c5
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2022-05-18 21:57:25 +0200

    Add 3 components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Image from './components/Image';
+import Main from './components/main';
+
+function App() {
+  return (
+    <div className="App">
+      <Image />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Image.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './image.css';
+
+function Image () {
+    return (
+        <div className="img">
+
+        </div>
+    )
+}
+export default Image
